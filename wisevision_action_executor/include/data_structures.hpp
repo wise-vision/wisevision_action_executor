000511d8fdfdc7829f977cd663a2d5cc423c4965
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-01-23 15:22:57 +0100

    Add license

--- wisevision_action_executor/include/data_structures.hpp
+++ wisevision_action_executor/include/data_structures.hpp
@@ -1,4 +1,12 @@
-// Copyright (c) 2024, WiseVision. All rights reserved.
+/*
+ * Copyright (C) 2025 wisevision
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 #ifndef DATA_STRUCTURES_HPP
 #define DATA_STRUCTURES_HPP
 
